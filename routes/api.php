9e7ca6b81e89f769ef4fbba1ed96474ac72d7fb0
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-22 23:43:10 +0600

    all routes removed

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,3 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
