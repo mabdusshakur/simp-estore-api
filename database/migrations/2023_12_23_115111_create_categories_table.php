329656aac40897edc82b92e544e6d91a57941ac1
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-23 23:20:51 +0600

    necessary tables added

--- database/migrations/2023_12_23_115111_create_categories_table.php
+++ database/migrations/2023_12_23_115111_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('products_count')->default(0);
+            $table->integer('sub_categories_count')->default(0);
             $table->timestamps();
         });
     }
