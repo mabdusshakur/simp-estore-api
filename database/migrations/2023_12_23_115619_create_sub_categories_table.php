34e0573820cad311bbb9852c7e823c74564734a6
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-23 23:29:20 +0600

    slug field added

--- database/migrations/2023_12_23_115619_create_sub_categories_table.php
+++ database/migrations/2023_12_23_115619_create_sub_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
             $table->integer('products_count')->default(0);
             $table->foreignId('category_id')->references('id')->on('categories')->cascadeOnDelete();
             $table->timestamps();
