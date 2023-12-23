453e62fc9fc1b1527a65dbb4898358057c1530a9
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-23 16:14:37 +0600

    necessary fields added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 150);
             $table->string('email')->unique();
+            $table->string('phone_number', 20)->unique();
+            $table->string('address_1')->nullable();
+            $table->string('address_2')->nullable();
+            $table->string('city')->nullable();
+            $table->integer('country')->nullable();
+            $table->integer('postal_code')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->timestamps();
