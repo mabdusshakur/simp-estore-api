8619fc713b6f709ad61d2e9b27ac5d536acc2ff8
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-23 16:02:00 +0600

    login register route added

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
