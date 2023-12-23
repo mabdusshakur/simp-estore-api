beb27493ba8d4642addfd60e484edf771be2a377
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-23 16:04:55 +0600

    v1 prefix added

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+});
