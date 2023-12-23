a3f22dece73b4e9152376191c6e8e5db7359f568
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-23 16:55:10 +0600

    is_admin casted to boolean

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,5 +49,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'is_admin' => 'boolean',
     ];
 }
