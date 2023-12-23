91d33913bb1699d89effdd75b5de063e6eefb37d
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-24 00:24:40 +0600

    rules added

--- app/Http/Requests/UpdateSubCategoryRequest.php
+++ app/Http/Requests/UpdateSubCategoryRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class UpdateSubCategoryRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class UpdateSubCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +25,8 @@ class UpdateSubCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255', Rule::unique('sub_categories')->ignore($this->sub_category, 'id')],
+            'category_id' => 'required|integer|exists:categories,id',
         ];
     }
 
