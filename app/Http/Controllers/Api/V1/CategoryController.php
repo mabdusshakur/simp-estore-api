bdfc7e317a4492fd8ea9089df7fece8d8c7005ef
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-24 00:27:05 +0600

    response message structure changed

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -38,7 +38,7 @@ class CategoryController extends Controller
                 'name' => $request->name,
                 'slug' => Str::slug($request->name),
             ]);
-            return new CategoryResource([$category, 'message' => 'Category created successfully']);
+            return new CategoryResource([$category, 'status' => 'success', 'message' => 'Category created successfully']);
         } catch (\Throwable $th) {
             return response()->json([
                 'data' => [
@@ -84,7 +84,7 @@ class CategoryController extends Controller
                 'name' => $request->name,
                 'slug' => Str::slug($request->name),
             ]);
-            return new CategoryResource([$category, 'message' => 'Category updated successfully']);
+            return new CategoryResource([$category, 'status' => 'success', 'message' => 'Category updated successfully']);
         } catch (\Throwable $th) {
             return response()->json([
                 'data' => [
