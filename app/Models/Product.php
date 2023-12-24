373952fdcb22bcfc0ab6ad09a083e0a86ff504c8
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-24 22:53:59 +0600

    fillable properties defined

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'name',
+        'slug',
+        'description',
+        'regular_price',
+        'sale_price',
+        'category_id',
+        'subcategory_id',
+        'status',
+        'stock',
+        'view_count',
+        'sold_count',
+    ];
 }
