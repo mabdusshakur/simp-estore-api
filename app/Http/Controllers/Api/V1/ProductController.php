45a3dba9140e6f7f8c223053ec170ca12ff2fe2d
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-24 23:13:25 +0600

    store product added

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 
@@ -31,7 +32,28 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
-        //
+        try {
+            $request->validated();
+            $product = Product::create([
+                'name' => $request->name,
+                'slug' => Str::slug($request->name),
+                'description' => $request->description,
+                'regular_price' => $request->regular_price,
+                'sale_price' => $request->sale_price,
+                'category_id' => $request->category_id,
+                'subcategory_id' => $request->subcategory_id,
+                'status' => $request->status,
+                'stock' => $request->stock,
+            ]);
+            return new ProductResource([$product, 'status' => 'success', 'message' => 'Product created successfully']);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'data' => [
+                    'status' => 'error',
+                    'message' => $th->getMessage(),
+                ],
+            ], 500);
+        }
     }
 
     /**
