9873fc3092be365cfdd7c2f7a81423b075de6159
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-26 20:11:41 +0600

    namespace updated

--- app/Http/Controllers/Api/V1/CartController.php
+++ app/Http/Controllers/Api/V1/CartController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\v1;
 
 use App\Models\Cart;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCartRequest;
 use App\Http\Requests\UpdateCartRequest;
 
