a8fe97cbb172773d01ac05b9c7dc3fd9cabe888d
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-26 11:23:52 +0600

    wishlist necessary fields added

--- database/migrations/2023_12_26_052059_create_wishlists_table.php
+++ database/migrations/2023_12_26_052059_create_wishlists_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreignId('product_id')->references('id')->on('products')->cascadeOnDelete();
             $table->timestamps();
         });
     }
