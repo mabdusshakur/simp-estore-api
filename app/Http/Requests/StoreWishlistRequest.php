aade01d5c3c189a1e3505c5d059467630574b8f7
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-26 11:37:47 +0600

    user_id option removed

--- app/Http/Requests/StoreWishlistRequest.php
+++ app/Http/Requests/StoreWishlistRequest.php
@@ -24,7 +24,6 @@ class StoreWishlistRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user_id' => 'required|integer|exists:users,id',
             'product_id' => 'required|integer|exists:products,id',
         ];
     }
