159d91e54d0588cf1500675d1bb9ef650efc7fc9
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-27 17:53:01 +0600

    passed to orderItem resource

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -21,7 +21,7 @@ class OrderResource extends JsonResource
             'payment_method' => $this->payment_method,
             'transaction_id' => $this->transaction_id,
             'user_id' => $this->user_id,
-            'order_items' => OrderItems::where('order_id', $this->id)->get(),
+            'order_items' => OrderItemResource::collection(OrderItems::where('order_id', $this->id)->get()),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
 
