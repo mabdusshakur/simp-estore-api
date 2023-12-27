35684ccc6ddd4ee4e9d25477f9569519cbca409f
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-27 17:12:22 +0600

    necessary fields added

--- database/migrations/2023_12_27_110649_create_orders_table.php
+++ database/migrations/2023_12_27_110649_create_orders_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->decimal('total');
+            $table->string('status')->default('pending');
+            $table->string('payment_method');
+            $table->string('transaction_id')->nullable();
             $table->timestamps();
         });
     }
