dc9a11d2d71a48f4abb894180b1b55c99767ec2e
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-27 14:26:48 +0600

    request updated

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -32,7 +32,7 @@ class StoreProductRequest extends FormRequest
             'subcategory_id' => 'required|exists:sub_categories,id',
             'status' => 'required|boolean',
             'stock' => 'required|integer',
-            'images' => 'required|image|mimes:jpeg,png,jpg|max:2048'
+            'images' => 'required'
         ];
     }
 
