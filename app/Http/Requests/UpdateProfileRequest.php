ca8b881f975803da0d5f4d961be34a7e668ee1a5
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-27 16:30:00 +0600

    update profile request added

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|required|string|max:255',
+            'email' => ['sometimes', 'required', 'email', 'max:255', Rule::unique('users')->ignore(auth()->user()->id)],
+            'phone_number' => ['sometimes', 'required', 'numeric', Rule::unique('users')->ignore(auth()->user()->id)],
+            'address_1' => 'sometimes|required|string|max:255',
+            'address_2' => 'sometimes|required|string|max:255',
+            'city' => 'sometimes|required|string|max:255',
+            'postal_code' => 'sometimes|required|numeric',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'data' => [
+                'status' => 'error',
+                'message' => $validator->errors(),
+            ],
+        ], 401));
+    }
+}
