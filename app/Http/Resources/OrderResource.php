bd656532e5f9ee50140197eab5519274abaac4af
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-27 17:51:09 +0600

    order resource customized with orderItems

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\OrderItems;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,16 @@ class OrderResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'status' => $this->status,
+            'payment_method' => $this->payment_method,
+            'transaction_id' => $this->transaction_id,
+            'user_id' => $this->user_id,
+            'order_items' => OrderItems::where('order_id', $this->id)->get(),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+        ];
     }
 }
