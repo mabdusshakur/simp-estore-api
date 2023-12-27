722fa3a8279efa184eaf9e1446ef5d3d6abc393a
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-27 17:43:50 +0600

    Store order with order items added

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Models\Cart;
 use App\Models\Order;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
+use App\Models\OrderItems;
 
 class OrderController extends Controller
 {
@@ -30,7 +32,55 @@ class OrderController extends Controller
      */
     public function store(StoreOrderRequest $request)
     {
-        //
+        try {
+            $request->validated();
+            $cartItems = Cart::where('user_id', auth()->user()->id)->get();
+            $totalPrice = 0;
+            foreach ($cartItems as $cartItem) {
+                $totalPrice += $cartItem->product->sale_price ?? $cartItem->product->regular_price * $cartItem->quantity;
+            }
+
+            if($request->payment_method == 'cod')
+            {
+                $order = Order::create([
+                    'user_id' => auth()->user()->id,
+                    'total' => $totalPrice,
+                    'status' => 'pending',
+                    'payment_method' => $request->payment_method,
+                ]);
+                if($order)
+                {
+                    foreach ($cartItems as $cartItem) {
+                        $orderItem = new OrderItems();
+                        $orderItem->order_id = $order->id;
+                        $orderItem->product_id = $cartItem->product_id;
+                        $orderItem->quantity = $cartItem->quantity;
+                        $orderItem->price = $cartItem->product->sale_price ?? $cartItem->product->regular_price;
+                        $orderItem->save();
+                        $cartItem->product->stock -= $cartItem->quantity;
+                        $cartItem->product->sold_count += $cartItem->quantity;
+                        $cartItem->product->save();
+                        $cartItem->delete();
+                    }
+
+                    return response()->json([
+                        'data' => [
+                            'status' => 'success',
+                            'message' => 'Order created successfully',
+                            'order' => $order,
+                        ],
+                    ], 201);
+                }
+            }
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'data' => [
+                    'status' => 'error',
+                    'message' => $th->getMessage(),
+                ],
+            ], 500);
+        }
     }
 
     /**
