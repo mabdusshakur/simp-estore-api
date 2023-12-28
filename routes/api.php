77e4f912d566c9f92d644ef906cc6cef4be2a5d6
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-28 14:56:15 +0600

    resource api route added on admin

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,7 @@ Route::group(['prefix' => 'v1'], function () {
             Route::apiResource('sub-categories', \App\Http\Controllers\Api\v1\SubCategoryController::class);
             Route::apiResource('products', \App\Http\Controllers\Api\v1\ProductController::class);
             Route::post('products/delete-image', [\App\Http\Controllers\Api\v1\ProductController::class, 'deleteImage']);
+            Route::apiResource('orders', \App\Http\Controllers\Api\v1\OrderController::class);
         });
     });
 });
