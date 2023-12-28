35063f3ad3b4c761e0bd72f7b678b7a3b1a6c714
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-28 14:38:16 +0600

    order status mail template

---
+++ resources/views/emails/order-status-mail.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Order Status Update</title>
+</head>
+<body>
+    <h1>Order Status Update</h1>
+
+    <p>Dear {{ $mail_data['customer_name'] }},</p>
+
+    <p>We are writing to inform you about the status of your order.</p>
+
+    <table>
+        <tr>
+            <td>Order ID:</td>
+            <td>{{ $mail_data['order_id'] }}</td>
+        </tr>
+        <tr>
+            <td>Order Status:</td>
+            <td>{{ $mail_data['order_status'] }}</td>
+        </tr>
+        <tr>
+            <td>Order Date:</td>
+            <td>{{ $mail_data['order_date'] }}</td>
+        </tr>
+    </table>
+
+    <p>Thank you for shopping with us.</p>
+
+    <p>Best Regards,</p>
+    <p>{{config('app.name')}}</p>
+</body>
+</html>
