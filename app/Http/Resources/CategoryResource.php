449df90d4b03dad37213817a011f9371515a2ff0
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-31 13:45:49 +0600

    sub-categories resource added on categories

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,14 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'products_count' => $this->products_count,
+            'sub_categories_count' => $this->sub_categories_count,
+            'sub_categories' => SubCategoryResource::collection(SubCategory::where('category_id', $this->id)->get()),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
