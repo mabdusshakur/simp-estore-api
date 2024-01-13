78f821500ce4c4582fea2245ba2f40970082ef83
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-13 14:22:58 +0600

    product custom resource added

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,22 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name ?? '',
+            'slug' => $this->slug ?? '',
+            'description' => $this->description ?? '',
+            'regular_price' => $this->regular_price ?? '',
+            'sale_price' => $this->sale_price ?? '',
+            'category_id' => $this->category_id ?? '',
+            'subcategory_id' => $this->subcategory_id ?? '',
+            'status' => $this->status ?? '',
+            'stock' => $this->stock ?? '',
+            'view_count' => $this->view_count ?? '',
+            'sold_count' => $this->sold_count ?? '',
+            'images' => ImageResource::collection($this->images),
+            'category' => new CategoryResource($this->category),
+            'subcategory' => new SubCategoryResource($this->subcategory),
+        ];
     }
 }
