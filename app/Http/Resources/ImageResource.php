7cddd5a05e0b4be493efc53b441f2d3066cd4cee
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-13 14:52:41 +0600

    modified image resource added

--- app/Http/Resources/ImageResource.php
+++ app/Http/Resources/ImageResource.php
@@ -14,6 +14,10 @@ class ImageResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'path' => $this->path,
+            'product_id' => $this->product_id,
+        ];
     }
 }
