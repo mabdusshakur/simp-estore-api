5ec767416a7e946e22e9bba5153e1e4475583b8a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-17 10:17:27 +0600

    with product wishlist sent

--- app/Http/Resources/WishlistResource.php
+++ app/Http/Resources/WishlistResource.php
@@ -14,6 +14,11 @@ class WishlistResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'product_id' => $this->product_id,
+            'product' => new ProductResource($this->product),
+        ];
     }
 }
