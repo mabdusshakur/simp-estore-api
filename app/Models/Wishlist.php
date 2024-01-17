275272702e242e7d4874c52c0590f79ba8a49ec2
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-17 10:16:07 +0600

    product - user relation added

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -12,4 +12,14 @@ class Wishlist extends Model
         'user_id',
         'product_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
