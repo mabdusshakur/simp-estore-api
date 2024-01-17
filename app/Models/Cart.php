79ca15925c9723d51438d535cee1a0dc5f01d935
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-17 10:30:04 +0600

    user relation added

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -13,4 +13,8 @@ class Cart extends Model
     public function product(){
         return $this->belongsTo(Product::class);
     }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
